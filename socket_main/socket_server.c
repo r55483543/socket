ab5c6afb2057aff71b969c0387440ea6e13beab1
r55483543 <r55483543@yahoo.com.tw>
2017-12-04 22:12:32 +0800

    Initial commit

---
+++ socket_main/socket_server.c
@@ -0,0 +1,141 @@
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<dirent.h> 
+#include<stdlib.h> 
+#include <sys/stat.h> 
+ 
+int printDirFile(); 
+ 
+ 
+int main(int argc , char *argv[])
+{
+    int socket_desc , c, numbytes;
+    struct sockaddr_in server ;
+    //char *message;
+	//char fileName[128];
+	char* fileName = "Desert.jpg";
+	struct stat filestat;
+	char buf[1000000];
+	FILE *fp;
+
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("bind failed");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+	while(1){
+		int new_socket;
+		struct sockaddr_in  client;
+		//Accept and incoming connection
+		puts("Waiting for incoming connections...");
+		c = sizeof(struct sockaddr_in);
+		new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+		if (new_socket<0)
+		{
+			perror("accept failed");
+			return 1;
+		}
+		 
+		puts("Connection accepted");
+		printDirFile();
+		//memset(fileName, '\0', sizeof(fileName));
+		//printf("Select a file\n");
+		//scanf("%s",fileName);
+		if ( lstat(fileName, &filestat) < 0){
+			exit(1);
+		}
+	printf("The file size of server send is %lun", filestat.st_size);
+
+	fp = fopen(fileName, "rb");
+
+
+	//Sending file
+	while(!feof(fp)){
+		numbytes = fread(buf, sizeof(char), sizeof(buf), fp);
+		printf("fread %d bytes, \n", numbytes);
+		numbytes = write(new_socket, buf, numbytes);
+		printf("Sending %d bytesn\n",numbytes);
+	}
+	fclose(fp);
+	close(new_socket);
+
+    /*
+		if( recv(new_socket, &ret , sizeof(ret) , 0) < 0)
+		{
+			puts("receive fail");
+		}
+		else
+		{
+			client_event = ntohl(ret);
+			if(client_event == 1)
+			{	
+				printDirFile();
+			}
+			else if(client_event == 2)
+			{
+				message = "Desert.jpg";
+				if(send(socket_desc , message , strlen(message) , 0) < 0)
+				{
+					puts("Send failed");
+					return 1;
+				}
+			}
+			else{}
+		}
+		printf("client_event = %d\n",client_event);
+		//close(new_socket);
+		*/
+	}
+	
+	
+    //Reply to the client
+    //message = "Hello Client , socket server close, bye\n";
+    //write(new_socket , message , strlen(message));
+	printf("Hello Client , socket server close, bye\n");
+	close(socket_desc);
+     
+    return 0;
+}
+  
+int printDirFile()  
+{  
+    DIR *dirp;  
+    struct dirent *direntp;  
+    dirp = opendir("/home/root");  
+    if(dirp != NULL)  
+    {  
+        while(1)  
+        {  
+            direntp = readdir(dirp);  
+            if(direntp == NULL)  
+                break;  
+            else if(direntp->d_name[0] != '.')  
+                printf("%s\n", direntp->d_name);  
+        }  
+        closedir(dirp);  
+        return 0;  
+    }  
+  
+    return 0;  
+}  
---
+++ socket_r/socket_receive.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<sys/stat.h> 
+#include<stdlib.h> 
+ 
+#define FOLDER_PATH "/home/root/client/"
+int connectServer(int* fdSocket);
+int receiveFileName(int socket_desc, char* fileName);
+int receiveFile(int socket_desc, FILE* fp);
+
+static int socket_desc;
+
+int main(int argc , char *argv[])
+{
+    //int command = 0;
+	//int event = 0;
+	int socket_desc_x;
+	char server_fileName[128];
+	//char folder_path[30];
+	int numbytes;
+	char buf[1000000];
+	FILE *fp;
+	struct stat filestat;
+
+		printf("Input a file name for store\n");
+		memset(server_fileName, '\0', sizeof(server_fileName));
+		
+		scanf("%s",server_fileName);
+		
+		connectServer(&socket_desc_x);
+		
+		
+		//event = htonl(command);
+
+		if ( (fp = fopen(server_fileName, "wb")) == NULL){
+			printf("open file fail\n");
+			perror("fopen");
+			exit(1);
+		}
+		while(1){
+			numbytes = read(socket_desc, buf, sizeof(buf));
+			printf("read %d bytes, \n", numbytes);
+			if(numbytes == 0)
+			{
+				printf("Client receice success\n");
+				break;
+			}
+			numbytes = fwrite(buf, sizeof(char), numbytes, fp);
+			printf("fwrite %d bytesn\n", numbytes);
+		}			
+		fclose(fp);	
+		if ( lstat(server_fileName, &filestat) < 0){
+			exit(1);
+		}
+		printf("The file size of client receive is %lun", filestat.st_size);
+	
+    return 0;
+}
+
+int connectServer(int* fdSocket)
+{
+	//int socket_desc;
+    struct sockaddr_in server;
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+         
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+	
+		//Connect to remote server
+		if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+		{
+			puts("connect error");
+			return -1;
+		}
+		 
+		puts("Connected\n");
+		//*fdSocket = socket_desc;
+		return 0;
+}
+
+
+int receiveFileName(int socket_desc, char* fileName)
+{
+	char server_reply[128];
+	memset(server_reply, '\0', sizeof(server_reply));
+	while(1)
+	{
+		
+		if( recv(socket_desc, server_reply , 128 , 0) < 0)
+		{
+			puts("recv failed");
+		}
+		else
+		{
+			printf("File name is %s",server_reply);
+			break;
+		}
+	}
+	puts(server_reply);
+	//strncpy(fileName, server_reply, sizeof(server_reply));
+	//printf("File name is %s",fileName);
+	return 0;
+}
+
+int receiveFile(int socket_desc, FILE* fp)
+{
+	int numbytes;
+	char buf[1000000];
+	while(1){
+		numbytes = read(socket_desc, buf, sizeof(buf));
+		printf("read %d bytes, ", numbytes);
+		if(numbytes == 0){
+			break;
+		}
+		numbytes = fwrite(buf, sizeof(char), numbytes, fp);
+		printf("fwrite %d bytesn", numbytes);
+	}
+	return 0;
+}
